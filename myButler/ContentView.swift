88e4f36d0a13f8c026746b202456853ce5978b2d
atomictan <atomictan@users.noreply.github.com>
2026-01-25 22:50:10 +0800

    Initial Commit

---
+++ myButler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myButler
+//
+//  Created by Hairong Yu on 1/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myButler/myButlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  myButlerApp.swift
+//  myButler
+//
+//  Created by Hairong Yu on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct myButlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
